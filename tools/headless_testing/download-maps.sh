8b4cd46f049ed0e7c232a11a773e522883752b9d
Citrine <salinecitrine@gmail.com>
2024-01-26 09:16:18 -0800

    wip

--- tools/headless_testing/download-maps.sh
+++ tools/headless_testing/download-maps.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 engine/*/pr-downloader --filesystem-writepath "$BAR_ROOT" --download-map "Full Metal Plate 1.5"
+
+for map_name in "$@"; do
+    engine/*/pr-downloader --filesystem-writepath "$BAR_ROOT" --download-map "$map_name"
+done
--- tools/headless_testing/start.sh
+++ tools/headless_testing/start.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-rm -rf $1/LuaUI/Config
-$1/engine/*/spring-headless --isolation --write-dir "$1" "$2"
+# Get a list of MAP_NAMEs from filenames in ./maps/*.sd7
+MAP_NAMES=($(find ./maps -name "*.sd7" -exec basename {} \; | sed 's/_/ /g'))
+
+for MAP_NAME in "${MAP_NAMES[@]}"; do
+  TEMP_FILE=$(mktemp)
+
+  sed "s/MapName=.*?;/MapName=$MAP_NAME;/g" startscript.txt > "$TEMP_FILE"
+
+  rm -rf $1/LuaUI/Config
+  $1/engine/*/spring-headless --isolation --write-dir "$1" "$TEMP_FILE"
+
+  rm "$TEMP_FILE"
+done
