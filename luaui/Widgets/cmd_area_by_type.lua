9e0526af0ac13103914d96d5d6fdf4f82fd93428
Citrine <salinecitrine@gmail.com>
2024-03-17 09:28:13 -0700

    -

--- luaui/Widgets/cmd_area_by_type.lua
+++ luaui/Widgets/cmd_area_by_type.lua
@@ -1,50 +1,79 @@
 function widget:GetInfo()
 	return {
-		name = "Set Target by Unit Type",
-		desc = "Hold down Alt and give an area command centered on a unit of the type to restrict the command to",
+		name = "Issue Area Commands Filtered by Unit Type",
+		desc = "Hold down Alt and give an area command centered on a unit to restrict the command to units of that type",
 		license = "GNU GPL, v2 or later",
-		layer = 0,
+		layer = 999999,
 		enabled = true
 	}
 end
 
-local function _distributeTargets_iter(state, i)
-	local sourceCount = #(state.sourceIDs)
-	local targetCount = #(state.targetIDs)
-
-	if i >= math.max(sourceCount, targetCount) then
-		return nil
+local function cmdIDString(cmdID)
+	if cmdID < 0 then
+		return "{Build " .. UnitDefs[-cmdID].translatedHumanName .. "} (" .. cmdID .. ")"
+	else
+		if CMD[cmdID] ~= nil then
+			return CMD[cmdID] .. " (" .. cmdID .. ")"
+		else
+			return "unknown command (" .. cmdID .. ")"
+		end
 	end
-
-	local sourceIndex = (i % sourceCount) + 1
-	local sourceSelectCount = math.floor(i / sourceCount) + 1
-
-	if not state.canSourceMultiple and sourceSelectCount > 1 then
-		return nil
-	end
-
-	local targetIndex = (i % targetCount) + 1
-	local targetSelectCount = math.floor(i / targetCount) + 1
-
-	if not state.canTargetMultiple and targetSelectCount > 1 then
-		return nil
-	end
-
-	i = i + 1
-
-	return i, state.sourceIDs[sourceIndex], state.targetIDs[targetIndex], sourceSelectCount, targetSelectCount
 end
 
-local function distributeTargets(sourceIDs, targetIDs, canSourceMultiple, canTargetMultiple)
-	return _distributeTargets_iter, {
-		sourceIDs = sourceIDs,
-		targetIDs = targetIDs,
-		canSourceMultiple = canSourceMultiple,
-		canTargetMultiple = canTargetMultiple,
-	}, 0
+local function unitIDString(unitID)
+	if not unitID then
+		return nil
+	end
+	local unitDefID = Spring.GetUnitDefID(unitID)
+	if UnitDefs[unitDefID] == nil then
+		return unitID .. " (" .. (unitDefID or "nil") .. ")"
+	end
+
+	local unitName = UnitDefs[unitDefID].translatedHumanName
+	if unitName ~= nil then
+		return unitID .. " (" .. unitName .. ")"
+	end
 end
 
---todo: highlight units as circle is drawn? check unit_smart_select.lua
+local function unitDefIDString(unitDefID)
+	if not unitDefID then
+		return nil
+	end
+	if UnitDefs[unitDefID] == nil then
+		return "invalid (" .. (unitDefID or "nil") .. ")"
+	end
+
+	local unitName = UnitDefs[unitDefID].translatedHumanName
+	if unitName ~= nil then
+		return unitName .. " (" .. unitDefID .. ")"
+	end
+end
+
+local function distributeTargets(callback, sourceIDs, targetIDs)
+	local sourceCount = #sourceIDs
+	local targetCount = #targetIDs
+
+	for i = 0, math.max(sourceCount, targetCount) do
+		callback(
+			sourceIDs[(i % sourceCount) + 1],
+			targetIDs[(i % targetCount) + 1],
+			math.floor(i / sourceCount) + 1,
+			math.floor(i / targetCount) + 1
+		)
+	end
+end
+
+local function distanceXZ(a, b)
+	return math.diag(a[1] - b[1], 0, a[3] - b[3])
+end
+
+local function map(tbl, callback)
+	local result = {}
+	for i = 1, #tbl do
+		result[i] = callback(tbl[i], i, tbl)
+	end
+	return result
+end
 
 local CMD_SET_TARGET = 34923
 
@@ -54,17 +83,47 @@ local CMD_SET_TARGET = 34923
 possible options or concerns:
 * can target enemies
 * can target allies
+* can target units
+* can target features (and how to only choose "similar" features)
 * distribute commands (always/never/space)
 * when to send shift
 ]]--
 local commandSpecs = {
-	[CMD_SET_TARGET] = {},
-	[CMD.RECLAIM] = {}, -- reclaim for units only
-	[CMD.LOAD] = {},
-	[CMD.REPAIR] = {},
-	[CMD.RESURRECT] = {}, -- check for feature rez name instead of unitdefid
-	[CMD.CAPTURE] = {},
-	[CMD.AREA_ATTACK] = {}, -- attack?
+	[CMD_SET_TARGET] = {
+		validTargetTypes = {
+			unit = true,
+		},
+	},
+	[CMD.RECLAIM] = {
+		validTargetTypes = {
+			unit = true,
+		},
+	}, -- reclaim for units only
+	[CMD.LOAD_UNITS] = {
+		validTargetTypes = {
+			unit = true,
+		},
+	},
+	[CMD.REPAIR] = {
+		validTargetTypes = {
+			unit = true,
+		},
+	},
+	[CMD.RESURRECT] = {
+		validTargetTypes = {
+			feature = true,
+		},
+	}, -- check for feature rez name instead of unitdefid
+	[CMD.CAPTURE] = {
+		validTargetTypes = {
+			unit = true,
+		},
+	},
+	[CMD.ATTACK] = {
+		validTargetTypes = {
+			unit = true,
+		},
+	},
 }
 
 --[[
@@ -73,21 +132,34 @@ existing widget summary
 	* alt -> enemy: restrict team
 	          ally: restrict type
 	* ctrl -> restrict team
-* load distributes commands
+* load distributes commands too
 ]]--
 
 local SpringGetUnitDefID = Spring.GetUnitDefID
 local SpringGetUnitAllyTeam = Spring.GetUnitAllyTeam
 
-local allyTeam = Spring.GetMyAllyTeamID()
-local gameStarted
+--todo: update this when necessary (maybe not often?)
+local myAllyTeam = Spring.GetMyAllyTeamID()
 
-local function createCommand(cmdID, cmdOpts, targetID, cmdIndex)
-	local newCmdOpts = {}
-	if #cmdIndex >= 1 or cmdOpts.shift then
-		newCmdOpts = { "shift" }
+local function findTargets(spec, targetType, targetID, cmdX, cmdZ, cmdRadius)
+	if not spec.validTargetTypes[targetType] then
+		return
 	end
-	return { cmdID, { targetID }, newCmdOpts }
+
+	local targetAllyTeamID = targetType == "unit" and SpringGetUnitAllyTeam(targetID) or nil
+	local targetUnitDefID = SpringGetUnitDefID(targetID)
+
+	local areaUnits = Spring.GetUnitsInCylinder(cmdX, cmdZ, cmdRadius)
+
+	local targetIDs = {}
+	for i = 1, #areaUnits do
+		local unitID = areaUnits[i]
+		if SpringGetUnitAllyTeam(unitID) ~= myAllyTeam and SpringGetUnitDefID(unitID) == targetUnitDefID then
+			targetIDs[#targetIDs + 1] = unitID
+		end
+	end
+
+	return targetIDs
 end
 
 local function maybeRemoveSelf()
@@ -98,42 +170,176 @@ end
 
 function widget:PlayerChanged(playerID)
 	maybeRemoveSelf()
+	myAllyTeam = Spring.GetMyAllyTeamID()
 end
 
 function widget:Initialize()
 	maybeRemoveSelf()
 end
 
+local function getTargetFilterType(targetType, targetID)
+	if targetType == "unit" then
+		return Spring.GetUnitDefID(targetID)
+	elseif targetType == "feature" then
+		local resName = Spring.GetFeatureResurrect(targetID)
+		if string.len(resName) > 0 then
+			return resName
+		end
+	end
+end
+
+local activeCmdState = nil
+function widget:MousePress(x, y, button)
+	if button ~= 1 then
+		activeCmdState = nil
+		return
+	end
+
+	local cmdIndex, cmdID, cmdType, cmdName = Spring.GetActiveCommand()
+
+	Spring.Echo("[MousePress] " .. table.toString({
+		pos = { x, y },
+		button = button,
+		cmd = {
+			cmdIndex = cmdIndex,
+			cmdID = cmdID,
+			cmdType = cmdType,
+			cmdName = cmdName,
+		}
+	}))
+
+	-- check for alt when drawing because it can change between mouse press and release
+	if commandSpecs[cmdID] then
+		local targetType, targetID = Spring.TraceScreenRay(x, y)
+		local _, worldPosition = Spring.TraceScreenRay(x, y, true, true)
+		activeCmdState = {
+			cmdID = cmdID,
+			position = { x, y },
+			targetType = targetType,
+			targetId = targetID,
+			targetFilterType = getTargetFilterType(targetType, targetID),
+			targetAllyTeamID = targetType == "unit" and SpringGetUnitAllyTeam(targetID) or nil,
+			position = worldPosition,
+		}
+	else
+		activeCmdState = nil
+	end
+end
+
+function widget:Update()
+	if activeCmdState == nil then
+		return
+	end
+
+	local cmdIndex, cmdID, cmdType, cmdName = Spring.GetActiveCommand()
+
+	if not commandSpecs[cmdID] then
+		activeCmdState = nil
+	end
+end
+
+function widget:DrawWorld()
+	if activeCmdState == nil then
+		return
+	end
+
+	local alt, ctrl, meta, shift = Spring.GetModKeyState()
+
+	if not alt then
+		return
+	end
+
+	local mx, my = Spring.GetMouseState()
+	local _, worldPosition = Spring.TraceScreenRay(mx, my, true, true)
+	local cmdRadius = distanceXZ(activeCmdState.position, worldPosition)
+
+	local targetIDs = findTargets(
+		commandSpecs[activeCmdState.cmdID],
+		activeCmdState.targetType,
+		activeCmdState.targetId,
+		activeCmdState.position[1],
+		activeCmdState.position[3],
+		cmdRadius
+	)
+
+	if not targetIDs then
+		return
+	end
+
+	gl.Color(1.0, 1.0, 1.0, 1.0)
+	for _, targetID in ipairs(targetIDs) do
+		local ux, uy, uz = Spring.GetUnitPosition(targetID)
+		gl.DrawGroundCircle(ux, 0, uz, UnitDefs[Spring.GetUnitDefID(targetID)].radius, 32)
+	end
+end
+
+function widget:DrawScreenEffects()
+	if activeCmdState == nil then
+		return
+	end
+
+	local mx, my = Spring.GetMouseState()
+	local _, worldPosition = Spring.TraceScreenRay(mx, my, true, true)
+	local cmdRadius = distanceXZ(activeCmdState.position, worldPosition)
+
+	gl.Color(1.0, 1.0, 1.0, 1.0)
+	gl.Text(string.format(
+		"%s | %s | %s | %s",
+		activeCmdState.targetType or "<none>",
+		unitIDString(activeCmdState.targetId) or "<none>",
+		unitDefIDString(activeCmdState.targetFilterType) or "<none>",
+		activeCmdState.targetAllyTeamID or "<none>"
+	), mx + 15, my - 12, 40, "ao")
+end
+
+local function createCommand(cmdID, cmdOpts, targetID, cmdIndex)
+	local newCmdOpts = {}
+	if #cmdIndex >= 1 or cmdOpts.shift then
+		newCmdOpts = { "shift" }
+	end
+	return { cmdID, { targetID }, newCmdOpts }
+end
+
 function widget:CommandNotify(cmdID, cmdParams, cmdOpts)
+	activeCmdState = nil
 	if not commandSpecs[cmdID] or #cmdParams ~= 4 or not cmdOpts.alt then
 		return
 	end
+
+	Spring.Echo("[CommandNotify] " .. table.toString({
+		cmdID = cmdIDString(cmdID),
+		cmdParams = cmdParams,
+		cmdOpts = cmdOpts,
+	}))
+
+	local spec = commandSpecs[cmdID]
 
 	local cmdX, cmdY, cmdZ = cmdParams[1], cmdParams[2], cmdParams[3]
 
 	local mouseX, mouseY = Spring.WorldToScreenCoords(cmdX, cmdY, cmdZ)
 	local targetType, targetId = Spring.TraceScreenRay(mouseX, mouseY)
 
-	if targetType ~= "unit" then
+	if not spec.validTargetTypes[targetType] then
 		return
 	end
 
 	local cmdRadius = cmdParams[4]
 
-	local filterUnitDefID = SpringGetUnitDefID(targetId)
-	local areaUnits = Spring.GetUnitsInCylinder(cmdX, cmdZ, cmdRadius)
+	local targetIDs = findTargets(
+		spec, targetType, targetId, cmdX, cmdZ, cmdRadius
+	)
 
-	local targetIDs = {}
-	for i = 1, #areaUnits do
-		local unitID = areaUnits[i]
-		if SpringGetUnitAllyTeam(unitID) ~= allyTeam and SpringGetUnitDefID(unitID) == filterUnitDefID then
-			targetIDs[#targetIDs + 1] = unitID
+	local cmdArray = map(targetIDs, function(targetID, index)
+		local newCmdOpts = {}
+		if index > 1 or cmdOpts.shift then
+			newCmdOpts = { "shift" }
 		end
-	end
+		return { cmdID, { targetID }, newCmdOpts }
+	end)
 
-	if #targetIDs > 0 then
+	if #cmdArray > 0 then
 		local selectedUnits = Spring.GetSelectedUnits()
-		Spring.GiveOrderArrayToUnitArray(selectedUnits, targetIDs)
+		Spring.GiveOrderArrayToUnitArray(selectedUnits, cmdArray)
 		return true
 	end
 end
